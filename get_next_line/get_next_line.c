457bd0c823e6664537400f5f558aedcf4f8403aa
Victor Giordani <vgiordan@student.42lausanne.ch>
2022-11-09 14:52:42 +0100

    Update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vgiordan <vgiordan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 13:18:08 by victorgiord       #+#    #+#             */
-/*   Updated: 2022/11/08 12:20:31 by vgiordan         ###   ########.fr       */
+/*   Updated: 2022/11/09 14:50:02 by vgiordan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ char	*read_text_to_buffer(int fd)
 	if (!buffer)
 		return (NULL);
 	red = read(fd, buffer, BUFFER_SIZE);
-	//printf("RED : %d\n", red);
-	//printf("BUFFER : %s\n", buffer);
+	//printf("\nRED : %d\n", red);
+	//printf("BUFFER : %s\n\n", buffer);
 	buffer[red] = '\0';
 	return (buffer);
 }
@@ -37,6 +37,8 @@ char	*add_buffer_to_line(char *line, int fd)
 	i = 0;
 	j = 0;
 	buffer = read_text_to_buffer(fd);
+	if (buffer == NULL)
+		return (NULL);
 	while (buffer[i] != '\n' && buffer[i] != '\0')
 		i++;
 	if (remains)
@@ -47,7 +49,7 @@ char	*add_buffer_to_line(char *line, int fd)
 	}
 	line = ft_strnjoin(line, buffer, i + 1);
 	free(buffer);
-	if (buffer[i] != '\n')
+	if (buffer[i] != '\n' && !buffer[i + 1])
 		return (add_buffer_to_line(line, fd));
 	remains = malloc(BUFFER_SIZE - i);
 	while (buffer[i++])
@@ -59,9 +61,11 @@ char	*get_next_line(int fd)
 {
 	char	*line;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = "";
-	//printf("Result : %s", add_buffer_to_line(line, fd));
-	return (NULL);
+	line = add_buffer_to_line(line, fd);
+	if (line == NULL)
+		return (NULL);
+	return (line);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,33 +6,28 @@
 /*   By: vgiordan <vgiordan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 00:05:34 by victorgiord       #+#    #+#             */
-/*   Updated: 2022/11/07 12:43:07 by vgiordan         ###   ########.fr       */
+/*   Updated: 2022/11/09 14:28:51 by vgiordan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	main(int argc, char const *argv[])
+int	main(void)
 {
 	int		fd;
-	char	*line;
 	int		i;
 
 	i = 0;
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
 		return (-1);
-	while (i < 4)
+	//printf("\n-----RESULT-----\n");
+	while (i < 1)
 	{
 		get_next_line(fd);
+		//printf("%s", get_next_line(fd));
 		i++;
 	}
-	//printf("%s\n", get_next_line(fd));
-	/*printf("STRLCAT\n");
-	char *s1 = "Victor";
-	char *s2 = "Giordani";
-	ft_strlcat(s1, s2, 5);
-	printf("%s\n", ft_strnjoin(s1, s2, 5));
-	strcpy(s1, s2);*/
+	//printf("\n-----------------\n");
 	return (0);
 }
